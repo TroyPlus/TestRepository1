c8199377965f816424921deb8d5a2c8edcb6581b
TroyPlus <m.suganth@troy-plus.co.uk>
2014-08-22 11:28:13 +0100

    Added solution
    Added solution to git folder.

---
+++ Troy.Data/Repository/IManufactureRepository.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Troy.Model;
+
+namespace Troy.Data.Repository
+{
+    public interface IManufactureRepository
+    {
+        bool InsertFileUploadDetails(List<Manufacture> manufacture);
+    }
+}
---
+++ Troy.Data/Repository/ManufactureRepository.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Troy.Data.DataContext;
+using Troy.Model;
+
+namespace Troy.Data.Repository
+{
+    public class ManufactureRepository : BaseRepository, IManufactureRepository
+    {
+        private ManufactureContext manufactureContext = new ManufactureContext();
+
+        public bool InsertFileUploadDetails(List<Manufacture> manufacture)
+        {
+            try
+            {
+                manufactureContext.Manufacture.AddRange(manufacture);
+                manufactureContext.SaveChanges();
+                return true;
+            }
+            catch (Exception ex)
+            {
+
+                return false;
+            }
+        }
+    }
+}
